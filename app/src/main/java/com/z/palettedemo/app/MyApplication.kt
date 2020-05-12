165d1bd0edfd50de1320413f881fa43a36f2c43d
zhangkangbin <7066586+zhangkangbin@users.noreply.github.com>
2020-05-12 09:56:52 +0800

    bugly集成

---
+++ app/src/main/java/com/z/palettedemo/app/MyApplication.kt
@@ -0,0 +1,17 @@
+package com.z.palettedemo.app
+
+import android.app.Application
+import com.tencent.bugly.Bugly
+
+/**
+ * @author by zhangkangbin
+ * on 2020/5/12
+ * 界面说明
+ */
+class MyApplication :Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Bugly.init(getApplicationContext(), AppKey.BUGLY_KEY_ID, false)
+    }
+}
